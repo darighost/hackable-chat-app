e61daeca81bb78c4afc95a9540cb7e40ac71a243
nomad <nomad>
2023-10-04 17:46:09 -0600

    fix bug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -71,12 +71,13 @@ function App() {
     }).then(newFunds => {
       setFunds(newFunds.funds);
     })
-  }, [])
-  const user  = parseJwt(document.cookie.split('jwt=')[1]).user
+  }, []);
   const jwt = Cookies.get('jwt');
   if (!jwt) {
-    window.location.href = '/login.html'
+    window.location.href = '/login.html';
   }
+  const user  = parseJwt(document.cookie.split('jwt=')[1]).user
+  
   return (
     <div className="App">
       <header className="App-header">
